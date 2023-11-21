5a55cddc716fe48adeeed5c153d0b782350b51ec
Domingo Uribe <uribe.domingo@gmail.com>
2023-11-21 18:13:38 -0300

    Colors defined

--- src/components/Header/Card/Card.jsx
+++ src/components/Header/Card/Card.jsx
@@ -4,11 +4,11 @@ export const Card = () => {
   return (
     <>
       <article className="Card">
-        <header>
+        <header className="Card-header">
           <span className="Card-tag">Your result</span>
-          <h2 className="Card-number">
+          <span className="Card-number">
             76 <span className="Card-info">of 100</span>
-          </h2>
+          </span>
           <span>Great</span>
           <p>
             You scored higher than 65% of the people who have taken these tests.
@@ -20,33 +20,30 @@ export const Card = () => {
             <li className="Data-item">
               <span>
                 <i></i>Reaction
-              </span>{" "}
+              </span>
               80 / 100
             </li>
             <li className="Data-item">
               <span>
                 <i></i>Reaction
-              </span>{" "}
+              </span>
               80 / 100
             </li>
             <li className="Data-item">
               <span>
                 <i></i>Reaction
-              </span>{" "}
+              </span>
               80 / 100
             </li>
             <li className="Data-item">
               <span>
                 <i></i>Reaction
-              </span>{" "}
+              </span>
               80 / 100
             </li>
           </ul>
-        </div>
-
-        <footer>
           <button className="Button">Continue</button>
-        </footer>
+        </div>
       </article>
     </>
   );
